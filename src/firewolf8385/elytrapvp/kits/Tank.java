907895b7f6330fe2ef1ec01dedf0e5c89490d310
firewolf8385 <colbykrawczyk10@gmail.com>
2018-06-06 19:56:49 -0400

    Added 8 kits:   - Bomber   - Chemist   - Healer   - Knight   - Pyro   - Sniper   - Stickman   - Tank

---
+++ src/firewolf8385/elytrapvp/kits/Tank.java
@@ -0,0 +1,50 @@
+package firewolf8385.elytrapvp.kits;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Tank {
+
+    public static void giveKit(Player p) {
+
+        ItemStack bow = new ItemStack(Material.BOW, 1);
+        ItemMeta bow2 = bow.getItemMeta();
+        bow2.setDisplayName(ChatColor.GOLD + "Tank Bow");
+        bow2.setUnbreakable(true);
+        bow2.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
+        bow2.addEnchant(Enchantment.ARROW_DAMAGE, 1, true);
+        bow.setItemMeta(bow2);
+
+        ItemStack fireworks = new ItemStack(Material.FIREWORK, 64);
+
+        ItemStack elytra = new ItemStack(Material.ELYTRA, 1);
+        ItemMeta elytrameta = elytra.getItemMeta();
+        elytrameta.setUnbreakable(true);
+        elytra.setItemMeta(elytrameta);
+
+        ItemStack arrow = new ItemStack(Material.ARROW, 1);
+
+        if (!(p.getInventory().contains(bow))) {
+            p.getInventory().clear();
+
+            p.getInventory().setItem(0, bow);
+
+            p.getInventory().setItemInOffHand(fireworks);
+
+            p.getInventory().setItem(38, elytra);
+
+            p.getInventory().setItem(17, arrow);
+
+            p.setMaxHealth(26);
+            p.setHealth(26);
+
+        }
+
+    }
+
+}
