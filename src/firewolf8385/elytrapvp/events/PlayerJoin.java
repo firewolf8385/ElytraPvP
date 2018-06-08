f4396c7b9cef0170aa14e18da6543b00bdbba8a9
firewolf8385 <colbykrawczyk10@gmail.com>
2018-06-07 20:03:45 -0400

    Updated Config.yml and Plugin.yml

---
+++ src/firewolf8385/elytrapvp/events/PlayerJoin.java
@@ -0,0 +1,84 @@
+package firewolf8385.elytrapvp.events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffectType;
+
+import firewolf8385.elytrapvp.PlayerData;
+import firewolf8385.elytrapvp.SettingsManager;
+
+
+
+public class PlayerJoin implements Listener{
+
+    SettingsManager settings = SettingsManager.getInstance();
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+
+        // Only Runs When The plugin Is Enabled
+        if(settings.getConfig().getBoolean("Enabled")) {
+            Player p = e.getPlayer();
+
+            PlayerData player = new PlayerData(p.getUniqueId());
+            player.createPlayerConfig();
+            player.createPlayerDefaults();
+            player.savePlayerConfig();
+
+            player.addLogins(1);
+            player.savePlayerConfig();
+
+            Double x = settings.getConfig().getDouble("Spawn.X");
+            Double y = settings.getConfig().getDouble("Spawn.Y");
+            Double z = settings.getConfig().getDouble("Spawn.Z");
+            Float pitch = (float) settings.getConfig().getDouble("Spawn.Pitch");
+            Float yaw = (float) settings.getConfig().getDouble("Spawn.Yaw");
+            World world = p.getWorld();
+
+            Location spawn = new Location(world, x, y, z, yaw, pitch);
+            p.teleport(spawn);
+
+            p.getInventory().clear();
+
+            ItemStack stats = new ItemStack(Material.PAPER);
+            ItemMeta stats2 = stats.getItemMeta();
+            stats2.setDisplayName(ChatColor.GOLD + "Stats");
+            stats.setItemMeta(stats2);
+
+            ItemStack kits = new ItemStack(Material.NETHER_STAR);
+            ItemMeta kits2 = kits.getItemMeta();
+            kits2.setDisplayName(ChatColor.GOLD + "Kit Selector");
+            kits.setItemMeta(kits2);
+
+            ItemStack upgrades = new ItemStack(Material.ARROW);
+            ItemMeta upgrades2 = upgrades.getItemMeta();
+            upgrades2.setDisplayName(ChatColor.GOLD + "Upgrades");
+            upgrades.setItemMeta(upgrades2);
+
+            ItemStack achievements = new ItemStack(Material.BOOK);
+            ItemMeta achievements2 = achievements.getItemMeta();
+            achievements2.setDisplayName(ChatColor.GOLD + "Achievements");
+            achievements.setItemMeta(achievements2);
+
+            p.getInventory().setItem(1, stats);
+            p.getInventory().setItem(4, kits);
+            p.getInventory().setItem(7, upgrades);
+            p.getInventory().setItem(8, achievements);
+
+            p.setMaxHealth(20.0);
+            p.setHealth(20.0);
+
+            p.removePotionEffect(PotionEffectType.REGENERATION);
+        }
+
+    }
+
+}
