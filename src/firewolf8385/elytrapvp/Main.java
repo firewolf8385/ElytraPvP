2b8dad782e07cfc1bcef117bdd6dbfebae015d49
firewolf8385 <colbykrawczyk10@gmail.com>
2018-06-08 16:12:39 -0400

    Updated Main Class

--- src/firewolf8385/elytrapvp/Main.java
+++ src/firewolf8385/elytrapvp/Main.java
@@ -1,5 +1,8 @@
 package firewolf8385.elytrapvp;
 
+import firewolf8385.elytrapvp.commands.ElytraPvP;
+import firewolf8385.elytrapvp.commands.Stats;
+import firewolf8385.elytrapvp.events.*;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -18,6 +21,18 @@ public class Main extends JavaPlugin{
         else {
             Bukkit.getLogger().warning("[ElytraPvP] PlaceholderAPI not found. Placeholders will not work without this!");
         }
+
+
+        getCommand("stats").setExecutor(new Stats());
+        getCommand("elytrapvp").setExecutor(new ElytraPvP());
+
+        getServer().getPluginManager().registerEvents(new EntityDamage(), this);
+        getServer().getPluginManager().registerEvents(new FoodLevelChange(), this);
+        getServer().getPluginManager().registerEvents(new PlayerInteract(), this);
+        getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
+        getServer().getPluginManager().registerEvents(new PlayerRespawn(), this);
+
+
     }
 
 }
