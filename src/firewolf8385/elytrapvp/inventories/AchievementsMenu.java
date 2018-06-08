0f8fe3e76a348338cbf9eec2eaaed60b15e7e044
firewolf8385 <colbykrawczyk10@gmail.com>
2018-06-08 16:12:16 -0400

    Started GUI classes

---
+++ src/firewolf8385/elytrapvp/inventories/AchievementsMenu.java
@@ -0,0 +1,17 @@
+package firewolf8385.elytrapvp.inventories;
+
+import org.bukkit.entity.Player;
+
+public class AchievementsMenu {
+
+    public static String mainGUI;
+
+    public static void openAcheievements(Player p){
+
+        // We are currently looking for Achievement ideas
+        // If you have any, let us know!
+
+    }
+
+
+}
---
+++ src/firewolf8385/elytrapvp/inventories/ShopMenu.java
@@ -0,0 +1,4 @@
+package firewolf8385.elytrapvp.inventories;
+
+public class ShopMenu {
+}
