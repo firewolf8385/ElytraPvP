a52013329d521d3fb721254c340f515867c43f0c
firewolf8385 <colbykrawczyk10@gmail.com>
2018-07-30 16:12:58 -0400

    Moved around items  in inventory and renamed the emerald to "cosmetics"

--- src/firewolf8385/elytrapvp/utils/SpawnItems.java
+++ src/firewolf8385/elytrapvp/utils/SpawnItems.java
@@ -20,19 +20,19 @@ public class SpawnItems {
         kits2.setDisplayName(ChatColor.GOLD + "Kit Selector");
         kits.setItemMeta(kits2);
 
-        ItemStack upgrades = new ItemStack(Material.EMERALD);
-        ItemMeta upgrades2 = upgrades.getItemMeta();
-        upgrades2.setDisplayName(ChatColor.GOLD + "Shop");
-        upgrades.setItemMeta(upgrades2);
+        ItemStack cosmetics = new ItemStack(Material.EMERALD);
+        ItemMeta cosmetics2 = cosmetics.getItemMeta();
+        cosmetics2.setDisplayName(ChatColor.GOLD + "Cosmetics");
+        cosmetics.setItemMeta(cosmetics2);
 
         ItemStack achievements = new ItemStack(Material.BOOK);
         ItemMeta achievements2 = achievements.getItemMeta();
         achievements2.setDisplayName(ChatColor.GOLD + "Achievements");
         achievements.setItemMeta(achievements2);
 
-        p.getInventory().setItem(1, stats);
+        p.getInventory().setItem(1, cosmetics);
         p.getInventory().setItem(4, kits);
-        p.getInventory().setItem(7, upgrades);
+        p.getInventory().setItem(7, stats);
         p.getInventory().setItem(8, achievements);
 
     }
